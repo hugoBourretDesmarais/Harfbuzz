f5f8b2357255bee2c4e818cce3753728ad0efbad
Hugo Bourret-Desmarais <hugob@unity3d.com>
2023-06-17 14:36:17 -0300

    Improve Renderer

--- Renderer.cpp
+++ Renderer.cpp
@@ -6,15 +6,23 @@ void Renderer::render_glyphs(const std::vector<FT_Bitmap*>& bitmaps)
 {
     // ASCII shades from light to dark
     const char* shades = " .:-=+*#%@";
+    const char padding_char = shades[9];
 
     // Iterate over all bitmap
     for (auto bitmap : bitmaps) {
         // Print bitmap dimensions
         printf("Bitmap dimensions: Width = %d, Rows = %d, Pitch = %d\n", 
             bitmap->width, bitmap->rows, bitmap->pitch);
+        
+        // Print top padding
+        for (int i = 0; i < bitmap->width + 2; i++) {
+            putchar(padding_char);
+        }
+        putchar('\n');
 
         // Print the bitmap content
         for (int i = 0; i < bitmap->rows; i++) {
+            putchar(padding_char); // left padding
             for (int j = 0; j < bitmap->width; j++) {
                 // Get the pixel brightness (0 = black, 255 = white)
                 unsigned char pixel = bitmap->buffer[i * bitmap->pitch + j];
@@ -25,7 +33,13 @@ void Renderer::render_glyphs(const std::vector<FT_Bitmap*>& bitmaps)
                 // Print the corresponding ASCII character
                 putchar(shades[shade_index]);
             }
+            putchar(padding_char); // right padding
             putchar('\n');
+        }
+
+        // Print bottom padding
+        for (int i = 0; i < bitmap->width + 2; i++) {
+            putchar(padding_char);
         }
 
         // Print a newline to separate the bitmaps
