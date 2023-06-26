4c52d9be9ba85f6e1ab7fc0db63d9e7ace0bf200
Hugo Bourret-Desmarais <hugob@unity3d.com>
2023-06-26 19:24:00 -0300

    Add OpenGL

--- main.cpp
+++ main.cpp
@@ -1,25 +1,24 @@
 #include "TextShapingPerformanceTester.h"
+#include <GL/glew.h> // Include the GLEW header file
+#include <GLFW/glfw3.h> // Include the GLFW header file
 
-int main()
-{
+void performTextShapingTest() {
     char* fontFile = "Resources/Roboto-Bold.ttf";
 
     // PERFORMANCE TESTS
-//    TextShapingPerformanceTester tester(fontFile);
+    // TextShapingPerformanceTester tester(fontFile);
+    // tester.shapeSameString(100, "ffi");
+    // tester.runRandomStringTests(1, 100);
+    // tester.runRandomStringOnlyShapingTests(10, 200);
 
-    ////tester.shapeSameString(100, "ffi");
-    ////tester.runRandomStringTests(1, 100);
-    //tester.runRandomStringOnlyShapingTests(10, 200);
+    // printf("Average time taken: %.10lf milliseconds\n", tester.getAverageTime());
+    // printf("Minimum time taken: %.10lf milliseconds\n", tester.getMinTime());
+    // printf("Maximum time taken: %.10lf milliseconds\n", tester.getMaxTime());
+    // printf("Median time taken: %.10lf milliseconds\n", tester.getMedianTime());
 
-    //printf("Average time taken: %.10lf milliseconds\n", tester.getAverageTime());
-    //printf("Minimum time taken: %.10lf milliseconds\n", tester.getMinTime());
-    //printf("Maximum time taken: %.10lf milliseconds\n", tester.getMaxTime());
-    //printf("Median time taken: %.10lf milliseconds\n", tester.getMedianTime());
-
-    //tester.printAllTimings();
+    // tester.printAllTimings();
 
     // Example using FT, HB, and Renderer
-
     FontHandler fh;
     fh.initFont(fontFile, false);
     fh.shape("ffi.", true);
@@ -28,29 +27,44 @@ int main()
     renderer.render_glyphs(fh.getGlyphs());
 
     fh.destroyFont();
+}
+
+int main() {
+    //performTextShapingTest();
+    // OpenGL Window creation and context set-up
+
+    GLFWwindow* window;
+
+    // Initialize the library
+    if (!glfwInit())
+        return -1;
+
+    // Create a windowed mode window and its OpenGL context
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+        return -1;
+
+    // Loop until the user closes the window
+    while (!glfwWindowShouldClose(window)) {
+        // Render here
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 
     return 0;
 }
-
-
-// hb_blob_t is a data type in HarfBuzz used to represent an opaque, 
-// reference-counted blob of binary data. It can be used to manage memory 
-// related to font data, handling operations like referencing, dereferencing, 
-// and destroying the memory when it is no longer needed.
-
-// hb_blob_create, hb_blob_get_length, hb_blob_get_data, hb_blob_destroy
-
-
-// Example of hb_blob_get_data 
-// unsigned int length;
-// hb_blob_t *blob = hb_blob_create_from_file("path/to/font/file");
-// const char *data = hb_blob_get_data(blob, &length);
-
-// Now you can read the font data from the "data" pointer. Remember not to go past "length" bytes.
-
-    //hb_blob_t *blob;
-    //blob = hb_blob_create("test", 4, HB_MEMORY_MODE_READONLY, NULL, NULL);
-    //unsigned int blob_length = hb_blob_get_length(blob);
-    //hb_blob_destroy(blob);
-
-    //return 0;
